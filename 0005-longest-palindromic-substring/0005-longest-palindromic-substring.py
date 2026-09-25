6d501cbddd03eae7c2a4890337a9c0db8bd36c72
Raghunath M N <reddyraghunathmn@gmail.com>
2026-09-25 19:50:44 +0530

    Time: 283 ms (53.24%), Space: 19.2 MB (70.91%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -11,6 +11,6 @@ class Solution:
         for i in range(n):
             b1 = expand_pallindrome(i,i)
             b2 = expand_pallindrome(i,i+1)            
-            res=max(res, b1, b2,key=lambda x: x[1]-x[0]+1) # find max based on the length of the pallindrome strings.
+            res=max(res, b1, b2,key=lambda x: x[1]-x[0]+1) 
                     
         return s[res[0]:res[1]]    
